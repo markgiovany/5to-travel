c78138ea809a3b2c42e99692550396264a9ecb8f
Julio Serrano Dominguez <ingjuliosd@gmail.com>
2026-04-19 21:41:19 -0500

    Ya muestras las nuevas hab añadidas
    seajusto lo de añadir habitacioners para que se vea en el Dashboard

--- propietario/agregar_habitacion.php
+++ propietario/agregar_habitacion.php
@@ -134,7 +134,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <html lang="es">
 <head>
 <meta charset="UTF-8">
-<title>Agregar Habitación PRO</title>
+<title>Agregar Habitación</title>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 
