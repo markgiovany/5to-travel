57642e06a04a7ee957699a14ad3a0d439d26b40c
Julio Serrano Dominguez <ingjuliosd@gmail.com>
2026-04-23 10:43:18 -0500

    Ya ahora si quedo
    Perfil al toque

--- auth/agregar_pago.php
+++ auth/agregar_pago.php
@@ -2,15 +2,25 @@
 session_start();
 include("../config/config.php");
 
+if (!isset($_SESSION['user_uuid'])) { 
+    header("Location: ../index.php");
+    exit(); 
+}
+
 $user_id = $_SESSION['user_uuid'];
 
-$tipo = $_POST['tipo'];
-$numero = $_POST['numero'];
+// Aquí ya no usamos "tipo", usamos el nombre del formulario
+$id_metodo = isset($_POST['id_metodo_pago']) ? (int)$_POST['id_metodo_pago'] : 1; 
+$titular   = mysqli_real_escape_string($config, $_POST['titular']);
+$numero    = mysqli_real_escape_string($config, preg_replace('/[^0-9]/', '', $_POST['numero']));
 
-mysqli_query($config, "
-INSERT INTO metodos_pago (user_id, tipo, numero)
-VALUES ('$user_id', '$tipo', '$numero')
-");
+// Insertamos en la tabla real: usr_billetera
+$query = "INSERT INTO usr_billetera (user_uuid, id_metodo_pago, nombre_titular, datos_encriptados, id_status)
+          VALUES ('$user_id', '$id_metodo', '$titular', '$numero', 1)";
 
-header("Location: ../perfil.php");
+if (mysqli_query($config, $query)) {
+    header("Location: ../perfil.php?pago=success");
+} else {
+    die("Error en la base de datos: " . mysqli_error($config));
+}
 ?>
--- auth/eliminar_pago.php
+++ auth/eliminar_pago.php
@@ -1,9 +1,20 @@
 <?php
+session_start();
 include("../config/config.php");
 
-$id = $_GET['id'];
+if (isset($_GET['id']) && isset($_SESSION['user_uuid'])) {
+    
+    $id = mysqli_real_escape_string($config, $_GET['id']);
+    $user_uuid = $_SESSION['user_uuid'];
 
-mysqli_query($config, "DELETE FROM metodos_pago WHERE id='$id'");
+    /* SOFT DELETE: No borramos físicamente, cambiamos el estatus a 2 */
+    /* La tabla correcta es usr_billetera e identificamos por id_metodo_guardado */
+    mysqli_query($config, "UPDATE usr_billetera 
+                           SET id_status = 2 
+                           WHERE id_metodo_guardado = '$id' 
+                           AND user_uuid = '$user_uuid'");
+}
 
-header("Location: ../perfil.php");
+header("Location: ../perfil.php?pago=suspendido");
+exit();
 ?>
