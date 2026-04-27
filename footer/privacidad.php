ceafa5bdb244108daeded7cd51effb6410dd7103
Luis Ledesma Gomez <luisledesmagomez22@hotmail.com>
2026-04-27 17:18:23 -0500

    footer

---
+++ footer/privacidad.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="../styles/footer-pages.css">
+</head>
+<body>
+
+<div class="page-container">
+    <h1>Política de privacidad</h1>
+
+<p>Recopilamos datos para mejorar tu experiencia.</p>
+
+<ul>
+    <li>No compartimos información sin consentimiento</li>
+    <li>Uso de cookies</li>
+</ul>
+</body>
+</html>
